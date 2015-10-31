f128235d9fe92cfc70a13c75903cee57a346804f
jm3565 <jnmunoz74@gmail.com>
2015-10-31 19:11:25 -0400

    refactor directives into its own module

--- js/app.js
+++ js/app.js
@@ -1,29 +1,9 @@
 (function(){
-	var app = angular.module('gemStore', []);
+	var app = angular.module('gemStore', ['store-directives']);
 	
 	app.controller('StoreController', function(){
 		this.products = gems;
 		this.ratings = ratings;
-	});
-	
-	// Control Directive
-	app.directive('productPanels', function(){
-		return {
-			restrict: 'E',
-			templateUrl: '../src/templates/product-panels.html',
-			controllerAs: 'panel',
-			controller: function(){
-				this.tab = 3;
-		
-				this.selectTab = function(setTab){
-					this.tab = setTab;	
-				};
-				
-				this.isSelected = function(checkTab){
-					return this.tab === checkTab;	
-				};
-			}	
-		};
 	});
 	
 	app.controller('ReviewController', function(){
@@ -38,28 +18,6 @@
 		
 		this._parseRating = function(rtng){
 			return rtng.split(" ")[0]
-		}
-	});
-	
-	// Name of the directives need to be in camelCase
-	app.directive('productDescription', function(){
-		return {
-			restrict: 'E',
-			templateUrl: '../src/templates/product-description.html'
-		};
-	});
-	
-	app.directive('productSpecs', function(){
-		return {
-			restrict: 'A',
-			templateUrl: '../src/templates/product-specs.html'
-		};
-	});
-	
-	app.directive('productReviews', function(){
-		return {
-			restrict: 'E',
-			templateUrl: '../src/templates/product-reviews.html'
 		}
 	});
 	
